86da30ed8bfbb8162d224f80bb72292dade71f8d
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2026-02-24 11:20:34 -0400

    chore: Minimizar codigo css para usar tailwind

--- linkedout/views.py
+++ linkedout/views.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Sección de perfil y autenticación
 def login(request):
-    return render(request, 'login.html.html')
+    return render(request, 'login.html')
 
 
 def register(request):
@@ -50,7 +50,21 @@ def comment_post(request, post_id):
 
 
 def messages_view(request):
-    return render(request, 'messages.html')
+    message_rows = [
+        {'name': 'Samantha Ramirez', 'message': _('Por supuesto'), 'time': '10:07 AM', 'bold': True},
+        {'name': 'Gustavo Berne', 'message': _('Tu: OK'), 'time': _('Lun'), 'bold': False},
+        {'name': 'Jose Campos', 'message': _('Tu: Creo que deberia cambiarse'), 'time': _('Vie'), 'bold': False},
+        {'name': 'Luisdavid Colina', 'message': _('Tu: Luego lo revisare'), 'time': _('Mie'), 'bold': False},
+        {'name': 'Gabriel Padilla', 'message': _('Hola, tengo una sugerencia'), 'time': _('Mar'), 'bold': False},
+        {'name': 'First Guy', 'message': _('LinkedOut es lo maximo'), 'time': _('Dom'), 'bold': False},
+        {'name': 'Second Guy', 'message': _('Tu: OK'), 'time': _('Dom'), 'bold': False},
+        {'name': 'Third Guy', 'message': '👍', 'time': _('Oct 9'), 'bold': False},
+    ]
+    return render(request, 'messages.html', {
+        'message_rows': message_rows,
+        'page_title': _('Mensajes'),
+        'show_bottom_nav': True,
+    })
 
 
 def chat_view(request, user_id):
