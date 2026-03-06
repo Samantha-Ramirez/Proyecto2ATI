4c9c202bd2fdce78580a076f1be57347b35599df
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2026-03-06 01:20:04 -0400

    fix: Arreglar migraciones

--- apps/linkedout/models.py
+++ apps/linkedout/models.py
@@ -41,3 +41,27 @@ class JobApplication(models.Model):
 
     class Meta:
         unique_together = [("job_offer", "applicant")]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+
+    PROFESSIONAL = 'professional'
+    COMPANY = 'company'
+    USER_TYPE_CHOICES = [
+        (PROFESSIONAL, ('Profesional')),
+        (COMPANY, ('Empresa')),
+    ]
+
+    user_type = models.CharField(max_length=20,
+                                 choices=USER_TYPE_CHOICES,
+                                 default=PROFESSIONAL)
+
+    professional_summary = models.TextField(null=True, blank=True)
+    education = models.TextField(null=True, blank=True)
+    experience = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
